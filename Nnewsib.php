a3144395be634bbd2d36b84805210c74bbe50279
Proba <vfilistovich@yandex.ru>
2017-01-02 19:05:54 +0300

    Ура! Мой сайт в интернете

---
+++ Nnewsib.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Сайт Филистовича В.А.</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="content">
+    <div id="header">
+        <a href="index.php">Главная</a>/
+		<a href="Nnewsib.php">от Нижнего до Новосибирска</a>/
+        <a href="station.php">красивые ЖД вокзалы</a>/		
+    </div>
+    <h1>Нижний Новгород и Новосибирск</h1>
+    <div id="center">        
+
+			<a href="img/Nishny-1.jpg"><img src="img/Nishny-1.jpg"></a>			
+			<a href="img/Nishny-2.jpg"><img src="img/Nishny-2.jpg"></a>
+			<a href="img/Nishny-3.jpg"><img src="img/Nishny-3.jpg"></a>
+			<a href="img/Newsib-1.JPG"><img src="img/Newsib-1.JPG"></a>
+			<a href="img/Newsib-2.JPG"><img src="img/Newsib-2.JPG"></a>
+			<a href="img/Newsib-3.JPG"><img src="img/Newsib-3.JPG"></a>
+			
+
+    </div>
+</div>
+<div id="footer">
+    Copyright &copy; 2016 <a href="https://geekbrains.ru/" target="_blank">GeekBrains</a>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Сайт Филистовича В.А.</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="content">
+    <div id="header">
+        <a href="#">Главная</a>/
+		<a href="Nnewsib.php">от Нижнего до Новосибирска</a>/
+        <a href="station.php">красивые ЖД вокзалы</a>/		
+    </div>
+    <h1>Личный сайт студента GeekBrains</h1>
+    <div id="center">
+        <img src="img/phil.jpg" id="avatar">
+        <div id="box_text">
+            <p>Добрый день. Меня зовут <b>Валерий Филистович</b>.
+                <i>После универа я некоторое время работал программистом, писал на языках типа fortran и basic, потом в основном работал сисадмином</i>. Сейчас у меня есть свободное время и я хочу освоить новые языки программирования с учётом современных технологий и инструментов.</p>
+
+            <p>Очень доволен что 'вырулил' на IT-портал, <a href="https://geekbrains.ru" target="_blank">GeekBrains</a>
+            преподаватели которого здорово помогают в учёбе. </p>
+			<p>Сам я учился в Нижнем Новгороде, а по окончании универа распределился в Новосибирск. Поэтому и фото мои об этих городах  и о связи между ними<a href="Nnewsib.php">от Нижнего до Новосибирска</a> и <a href="station.php">красивые ЖД вокзалы</a></p>
+        </div>
+    </div>
+</div>
+<div id="footer">
+    Copyright &copy; <?php echo date("Y"); ?> <a href="https://geekbrains.ru/" target="_blank">GeekBrains</a>
+</div>
+</body>
+</html>
